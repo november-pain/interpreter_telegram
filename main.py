0542512bca0624f4f6de541a186b1d435e2cd7a4
Andriy Sultanov <as.andreysultanov@gmail.com>
2019-11-15 11:04:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import logging
 import requests
 import re
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
