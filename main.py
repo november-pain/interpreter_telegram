4d8bf1f6ce31f72b06669b9b62d2cceb1779559f
Andriy Sultanov <as.andreysultanov@gmail.com>
2019-11-15 10:59:54 +0200

    =added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import requests
+import re
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+updater = Updater('1016464732:AAEoGlGEZfu4bWDfreOf7_d9zIu14E8K_wA', use_context=True)
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+
+updater.start_polling()
+updater.idle()
